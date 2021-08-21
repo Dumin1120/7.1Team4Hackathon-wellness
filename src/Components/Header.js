3705bfaf4fee9aa9c4141061cf4406fbeae14b74
Alexander <alexanderl96@yahoo.com>
2021-08-21 17:00:44 -0400

    home page

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <div>
+        <div className="header">
             <h1>Health Tech</h1>
         </div>
     )
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react'
 import { useHistory } from "react-router-dom"
+import images from "../images/Brain-illustration.jpeg"
+
 
 
 export default function Home({storeName}) {
@@ -15,13 +17,16 @@ export default function Home({storeName}) {
     }
 
     return (
-        <div>
-            <h1>HOME</h1>
+        <div className="Home">
+            <h1>Welcome</h1>
             <form action="" onSubmit={handleSubmit}>
             <label htmlFor="name">Name : </label>
             <input type="text" id="name" onChange={handleNameChange}/>
+            <br></br>
             <button type="submit">Submit</button>
             </form>
+            <img src={images} alt="new" />
+            <h4>Stop negative self-talk to reduce stress</h4>
         </div>
     )
 }
