043ab6f4916b8518a414390557d3e39ad9a2ed5a
Du Min Chen <dm.chen0113@gmail.com>
2021-08-22 13:12:36 -0400

    remove extra

--- src/App.js
+++ src/App.js
@@ -1,44 +1,14 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-import Home from "./Pages/Home";
-import Advice from "./Pages/Advice";
-import Affirmations from "./Pages/Affirmations";
-import Activities from "./Pages/Activities";
 import Sentiment from "./Pages/Sentiment";
 
 import Header from "./Components/Header";
 
 function App() {
-  const [user, setUser] = useState("anonymous");
-
-  const storeName = (userName) => {
-    setUser(userName);
-  };
   return (
     <div className="App">
-      <Router>
-        <Header />
-        <main>
-          <Switch>
-            <Route exact path="/">
-              <Home storeName={storeName} />
-            </Route>
-            <Route exact path="/sentiment">
-              <Sentiment user={user} />
-            </Route>
-            <Route exact path="/affirmations">
-              <Affirmations />
-            </Route>
-            <Route exact path="/advices">
-              <Advice />
-            </Route>
-            <Route exact path="/activities">
-              <Activities />
-            </Route>
-          </Switch>
-        </main>
-      </Router>
+      <Header />
+      <main>
+        <Sentiment />
+      </main>
     </div>
   );
 }
--- src/Pages/Sentiment.js
+++ src/Pages/Sentiment.js
@@ -8,7 +8,7 @@ import ActionProvider from "../Components/chatbot/ActionProvider";
 import "./Sentiment.css";
 
 const Sentiment = ({ user }) => {
-    const newConfig = { ...config, initialMessages: [createChatBotMessage(`${user}, how are you today?`)] };
+    const newConfig = { ...config, initialMessages: [createChatBotMessage(`Welcome to Unlocking Your Mind, make c`),] };
     return (
         <div className="sentiment">
             <div className="chatbot">
@@ -18,43 +18,6 @@ const Sentiment = ({ user }) => {
                     actionProvider={ActionProvider}
                 />
             </div>
-            <h3>How do you feel today “User”?</h3>
-            <form>
-                <input></input>
-                <div className="sentiment2">
-                    <button>Happy</button>
-                    <button>Surprise</button>
-                </div>
-                <div className="sentiment2">
-                    <button>Worried</button>
-                    <button>Thoughtful</button>
-                </div>
-                <div className="sentiment2">
-                    <button>amazed</button>
-                    <button>sad</button>
-                </div>
-                <div className="sentiment2">
-                    <button>Love</button>
-                    <button>Angry</button>
-                </div>
-                <div className="sentiment2">
-                    <button>Scared</button>
-                    <button>Tired</button>
-                </div>
-                <div className="sentiment2">
-                    <button>Bored</button>
-                    <button>Crazy</button>
-                </div>
-                <div className="sentiment2">
-                    <button>Relieved</button>
-                    <button>Hungry</button>
-                </div>
-                <div className="sentiment2">
-                    <button>Thirsty</button>
-                    <button>Shy</button>
-                </div>
-                <button className="sentiment3">Continue</button>
-            </form>
         </div>
     );
 };
